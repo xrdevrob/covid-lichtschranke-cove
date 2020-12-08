921a27d7ed5a168b00940e8ae0f565a13915f496
covierob <covierob@students.zhaw.ch>
2020-12-08 09:58:24 +0100

    update

--- dist/events.js
+++ dist/events.js
@@ -13,11 +13,9 @@ var app = new Vue({
       if (typeof EventSource !== "undefined") {
         var url = window.location.origin + "/api/events";
         var source = new EventSource(url);
-        var currentDate = new Date();
         source.onmessage = (event) => {
           this.messages.push(event.data);
           this.lastMessage = event.data;
-          this.timestamps = currentDate;
         };
       } else {
         this.message = "Your browser does not support server-sent events.";
